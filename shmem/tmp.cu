50604df4e9f040f9b37f86953c1017a39894af87
wpy <wpy@gpu1>
2020-11-07 17:10:35 +0800

    add

---
+++ shmem/tmp.cu
@@ -0,0 +1,61 @@
+#include "alias_table.cuh"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+template <typename T> __global__ void init_range_d(T *ptr, size_t size) {
+  if (TID < size) {
+    ptr[TID] = TID;
+  }
+}
+template <typename T> void init_range(T *ptr, size_t size) {
+  init_range_d<T><<<size / 512 + 1, 512>>>(ptr, size);
+}
+template <typename T> __global__ void init_array_d(T *ptr, size_t size, T v) {
+  if (TID < size) {
+    ptr[TID] = v;
+  }
+}
+template <typename T> void init_array(T *ptr, size_t size, T v) {
+  init_array_d<T><<<size / 512 + 1, 512>>>(ptr, size, v);
+}
+// todo
+/*
+1. prefix sum to normalize
+2.
+*/
+#define paster( n ) printf( "var: " #n " =  %d\n", n )
+int main(int argc, char const *argv[]) {
+
+  int *buf7;
+  int size = 40;
+
+  cudaSetDevice(1);
+  cudaMalloc(&buf7, size / 2 * sizeof(int));
+
+  int *id_ptr;
+  float *weight_ptr;
+  cudaMalloc(&id_ptr, size * sizeof(int));
+  cudaMalloc(&weight_ptr, size * sizeof(float));
+  init_range<int>(id_ptr, size);
+  init_array<float>(weight_ptr, size / 8 * 7, 0.5);
+  init_array<float>(weight_ptr + size / 8 * 7, size - size / 8 * 7, 2.0);
+
+  // P;
+  // alias_table<int> *table_ptr;
+  // alias_table<int> table_h;
+  Vector<int> out;
+  out.init(40);
+  paster(SHMEM_PER_WARP);
+  paster(TMP_PER_ELE);
+  paster(ELE_PER_WARP);
+
+  shmem_kernel<<<1, 32, 0, 0>>>(id_ptr, weight_ptr, size, size / 2, out);
+  // printf("size %d\n",sizeof(alias_table_shmem<int>));
+  // printf("size %d %d\n",sizeof(Vector_shmem<int>),ELE_PER_WARP);
+  P;
+  usleep(5000);
+  HERR(cudaDeviceSynchronize());
+  HERR(cudaPeekAtLastError());
+  return 0;
+}
---
+++ shmem/util.cu
@@ -0,0 +1,79 @@
+#include "util.cuh"
+
+// __device__ char char_atomicCAS(char *addr, char cmp, char val) {
+//   unsigned *al_addr = reinterpret_cast<unsigned *>(((unsigned long long)addr) &
+//                                                    (0xFFFFFFFFFFFFFFFCULL));
+//   unsigned al_offset = ((unsigned)(((unsigned long long)addr) & 3)) * 8;
+//   unsigned mask = 0xFFU;
+//   mask <<= al_offset;
+//   mask = ~mask;
+//   unsigned sval = val;
+//   sval <<= al_offset;
+//   unsigned old = *al_addr, assumed, setval;
+//   do {
+//     assumed = old;
+//     setval = assumed & mask;
+//     setval |= sval;
+//     old = atomicCAS(al_addr, assumed, setval);
+//   } while (assumed != old);
+//   return (char)((assumed >> al_offset) & 0xFFU);
+// }
+
+
+// template <typename T>
+// __inline__ __device__ T warpPrefixSum(T val, int lane_id) {
+//   T val_shuffled;
+//   for (int offset = 1; offset < warpSize; offset *= 2) {
+//     val_shuffled = __shfl_up(val, offset);
+//     if (lane_id >= offset) {
+//       val += val_shuffled;
+//     }
+//   }
+//   return val;
+// }
+__device__ void active_size(int n=0) {
+  coalesced_group active = coalesced_threads();
+  if (active.thread_rank() == 0)
+    printf("coalesced_group %d at line %d\n", active.size(),n);
+}
+template <typename T> void printH(T *ptr, int size) {
+  T *ptrh = new T[size];
+  HERR(cudaMemcpy(ptrh, ptr, size * sizeof(T), cudaMemcpyDeviceToHost));
+  printf("printH: ");
+  for (size_t i = 0; i < size; i++) {
+    // printf("%d\t", ptrh[i]);
+    std::cout << ptrh[i] << "\t";
+  }
+  printf("\n");
+  delete ptrh;
+}
+__device__ void printD(float *ptr, int size) {
+  printf("printDf: size%d, ", size);
+  for (size_t i = 0; i < size; i++) {
+    printf("%f\t", ptr[i]);
+  }
+  printf("\n");
+}
+__device__ void printD(int *ptr, int size) {
+  printf("printDi: size%d, ", size);
+  for (size_t i = 0; i < size; i++) {
+    printf("%d\t", ptr[i]);
+  }
+  printf("\n");
+}
+// template <typename T> __global__ void init_range_d(T *ptr, size_t size) {
+//   if (TID < size) {
+//     ptr[TID] = TID;
+//   }
+// }
+// template <typename T> void init_range(T *ptr, size_t size) {
+//   init_range_d<T><<<size / 512 + 1, 512>>>(ptr, size);
+// }
+// template <typename T> __global__ void init_array_d(T *ptr, size_t size, T v) {
+//   if (TID < size) {
+//     ptr[TID] = v;
+//   }
+// }
+// template <typename T> void init_array(T *ptr, size_t size, T v) {
+//   init_array_d<T><<<size / 512 + 1, 512>>>(ptr, size, v);
+// }
