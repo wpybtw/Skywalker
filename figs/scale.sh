5e8bb43e0fdbddea464770b6a4ac574f1a03e009
wpy <wpy@gpu1>
2021-01-30 10:52:50 +0800

    clean

--- figs/scale.sh
+++ figs/scale.sh
@@ -12,9 +12,9 @@ NV=(916428    4847571 3072627  39459923   22744077     50636151 124836180)
 # HD=(4             2   1     4         4       2           1) # uk-union rmat29 web-ClueWeb09)
 
 # DATA=( sk-2005 friendster) 
-# HD=(   4  1 )
+# HD=(   0.5           1 )
 ITR=1
-NG=8 #8
+NG=4 #8
 
 
 GR=".w.gr"
@@ -22,7 +22,9 @@ EXE="./bin/main" #main_degree
 SG="--ngpu=1 --s"
 RW="--rw=1 --k 1 --d 100 "
 SP="--rw=0 --k 20 --d 2 "
-BATCH="--n 40000"
+# BATCH="--n 40000"
+
+BATCH="--n 4"
 
 # --randomweight=1 --weightrange=2 
 
@@ -35,7 +37,14 @@ BATCH="--n 40000"
 #     done
 # done
 
-
+# echo "-------------------------------------------------------table" >> scale.csv
+# for idx in $(seq 1 ${#DATA[*]}) 
+# do
+#     for i in $(seq 1  ${NG})
+#     do
+#         ./bin/main -bias=1 --ol=0 --ngpu=$i --s ${RW} --input ~/data/${DATA[idx-1]}${GR} --hd=${HD[idx-1]} --n=4 >> scale.csv
+#     done
+# done
 
 # echo "-------------------------------------------------------offline rw 100" >> scale.csv
 # for idx in $(seq 1 ${#DATA[*]}) 
