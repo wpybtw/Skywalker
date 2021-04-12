abaea643d4900a4040431e2fbd6da7c656b5d843
wpy <wpy@gpu1>
2021-04-12 13:54:53 +0800

    buffer is good for 1hop but bad for 2hop?

--- src/unbiased_sample.cu
+++ src/unbiased_sample.cu
@@ -13,24 +13,20 @@ static __global__ void sample_kernel_first(Sampler_new *sampler, uint itr) {
   curandState state;
   curand_init(TID, 0, 0, &state);
   __shared__ matrixBuffer<BLOCK_SIZE, 10, uint> buffer_1hop;
-  // __shared__ matrixBuffer<BLOCK_SIZE, 25, uint> buffer_2hop; //not necessary
-  // __shared__ uint idxMap[BLOCK_SIZE];
-  // idxMap[LTID] = 0;
   buffer_1hop.Init();
-
   size_t idx_i = TID;
-  if (idx_i < result.size)  // for 2-hop, hop_num=3
+  if (idx_i < result.size)
   {
-    // idxMap[LTID] = idx_i;
     uint current_itr = 0;
     coalesced_group active = coalesced_threads();
-    // 1-hop
     {
       uint src_id = result.GetData(idx_i, current_itr, 0);
       uint src_degree = graph->getDegree((uint)src_id);
       uint sample_size = MIN(result.hops[current_itr + 1], src_degree);
       for (size_t i = 0; i < sample_size; i++) {
         uint candidate = (int)floor(curand_uniform(&state) * src_degree);
+        // *result.GetDataPtr(idx_i, current_itr + 1, i) =
+        //       graph->getOutNode(src_id, candidate);
         buffer_1hop.Set(
             graph->getOutNode(src_id, candidate));  // can move back latter
       }
@@ -40,69 +36,16 @@ static __global__ void sample_kernel_first(Sampler_new *sampler, uint itr) {
     }
   }
 }
-template <uint subwarp_size, bool doBuffer = false>
+template <uint subwarp_size>
 static __global__ void sample_kernel_second(Sampler_new *sampler,
-                                            uint current_itr) {}
-
-template <>
-static __global__ void sample_kernel_second<16,true>(Sampler_new *sampler,
                                             uint current_itr) {
   Jobs_result<JobType::NS, uint> &result = sampler->result;
   gpu_graph *graph = &sampler->ggraph;
   curandState state;
   curand_init(TID, 0, 0, &state);
-  size_t subwarp_id = TID / 16;
-  uint subwarp_idx = TID % 16;
-  uint local_subwarp_idx = LTID % 16;
-  bool alive = (subwarp_idx < result.hops[current_itr]) ? 1 : 0;
-  size_t idx_i = subwarp_id;  //
-
-  __shared__ uint buffer[BLOCK_SIZE][26];
-  __shared__ uint buffer_len[BLOCK_SIZE];
-  __shared__ uint idxMap[BLOCK_SIZE];
-  idxMap[LTID] = 0;
-  buffer_len[LTID] = 0;
-
-  if (idx_i < result.size)  // for 2-hop, hop_num=3
-  {
-    idxMap[LTID] = idx_i;
-    coalesced_group active = coalesced_threads();
-    {
-      uint src_id, src_degree, sample_size;
-      if (alive) {
-        src_id = result.GetData(idx_i, current_itr, subwarp_idx);
-        src_degree = graph->getDegree((uint)src_id);
-        sample_size = MIN(result.hops[current_itr + 1], src_degree);
-        for (size_t i = 0; i < sample_size; i++) {
-          uint candidate = (int)floor(curand_uniform(&state) * src_degree);
-          buffer[LTID][i] = graph->getOutNode(src_id, candidate);
-          buffer_len[LTID]++;
-        }
-      }
-      if (alive)
-        result.SetSampleLength(idx_i, current_itr, subwarp_idx, sample_size);
-      active.sync();
-      for (size_t i = 0; i < 32; i++) {  // LID id
-        for (size_t j = active.thread_rank(); j < buffer_len[(WID)*32 + i];
-             j++) {
-          *result.GetDataPtr(idxMap[(WID)*32 + i], current_itr + 1,
-                             active.thread_rank()) =
-              buffer[(WID)*32 + i][active.thread_rank()];
-        }
-      }
-    }
-  }
-}
-template <>
-static __global__ void sample_kernel_second<16,false>(Sampler_new *sampler,
-                                            uint current_itr) {
-  Jobs_result<JobType::NS, uint> &result = sampler->result;
-  gpu_graph *graph = &sampler->ggraph;
-  curandState state;
-  curand_init(TID, 0, 0, &state);
-  size_t subwarp_id = TID / 16;
-  uint subwarp_idx = TID % 16;
-  uint local_subwarp_idx = LTID % 16;
+  size_t subwarp_id = TID / subwarp_size;
+  uint subwarp_idx = TID % subwarp_size;
+  uint local_subwarp_idx = LTID % subwarp_size;
   bool alive = (subwarp_idx < result.hops[current_itr]) ? 1 : 0;
   size_t idx_i = subwarp_id;  //
 
@@ -272,7 +215,7 @@ float UnbiasedSample(Sampler_new &sampler) {
   if (FLAGS_peritr) {
     sample_kernel_first<<<sampler.result.size / BLOCK_SIZE + 1, BLOCK_SIZE, 0,
                           0>>>(sampler_ptr, 0);
-    sample_kernel_second<16,false>
+    sample_kernel_second<16>
         <<<sampler.result.size * 16 / BLOCK_SIZE + 1, BLOCK_SIZE, 0, 0>>>(
             sampler_ptr, 1);
   } else {
