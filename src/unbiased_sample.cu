535954150e9a5cfebb5f78d4a503c427aef4ad3b
wpy <wpy@gpu1>
2021-04-12 13:03:19 +0800

    peritr is good for 2hop sample;1.5x speedup

--- src/unbiased_sample.cu
+++ src/unbiased_sample.cu
@@ -41,39 +41,60 @@ static __global__ void sample_kernel_first(Sampler_new *sampler, uint itr) {
   }
 }
 template <uint subwarp_size>
-static __global__ void sample_kernel_second(Sampler_new *sampler, uint itr) {
+static __global__ void sample_kernel_second(Sampler_new *sampler,
+                                            uint current_itr) {
   Jobs_result<JobType::NS, uint> &result = sampler->result;
   gpu_graph *graph = &sampler->ggraph;
   curandState state;
   curand_init(TID, 0, 0, &state);
-  size_t subwarp_id = TID / subwarp_size;
-  size_t idx_i = subwarp_id;  //
 
-  __shared__ matrixBuffer<BLOCK_SIZE, 10, uint> buffer_1hop;
+  // __shared__ matrixBuffer<BLOCK_SIZE, 25, uint> buffer_1hop;
   // __shared__ matrixBuffer<BLOCK_SIZE, 25, uint> buffer_2hop; //not necessary
   // __shared__ uint idxMap[BLOCK_SIZE];
   // idxMap[LTID] = 0;
-  buffer_1hop.Init();
+  // buffer_1hop.Init();
+  size_t subwarp_id = TID / subwarp_size;
+  uint subwarp_idx = TID % subwarp_size;
+  uint local_subwarp_idx = LTID % subwarp_size;
+  bool alive = (subwarp_idx < result.hops[current_itr]) ? 1 : 0;
+  size_t idx_i = subwarp_id;  //
 
-  // size_t idx_i = TID;
+  __shared__ uint buffer[BLOCK_SIZE][25];
+  __shared__ uint buffer_len[BLOCK_SIZE];
+  __shared__ uint idxMap[BLOCK_SIZE];
+  idxMap[LTID] = 0;
+  buffer_len[LTID] = 0;
+
   if (idx_i < result.size)  // for 2-hop, hop_num=3
   {
+    idxMap[LTID] = idx_i;
     // idxMap[LTID] = idx_i;
-    uint current_itr = 0;
     coalesced_group active = coalesced_threads();
-    // 1-hop
     {
-      uint src_id = result.GetData(idx_i, current_itr, 0);
-      uint src_degree = graph->getDegree((uint)src_id);
-      uint sample_size = MIN(result.hops[current_itr + 1], src_degree);
-      for (size_t i = 0; i < sample_size; i++) {
-        uint candidate = (int)floor(curand_uniform(&state) * src_degree);
-        buffer_1hop.Set(
-            graph->getOutNode(src_id, candidate));  // can move back latter
+      uint src_id, src_degree, sample_size;
+      if (alive) {
+        src_id = result.GetData(idx_i, current_itr, subwarp_idx);
+        src_degree = graph->getDegree((uint)src_id);
+        sample_size = MIN(result.hops[current_itr + 1], src_degree);
+        for (size_t i = 0; i < sample_size; i++) {
+          uint candidate = (int)floor(curand_uniform(&state) * src_degree);
+          // buffer_1hop.Set(
+          //     graph->getOutNode(src_id, candidate));  // can move back latter
+          buffer[LTID][i] = graph->getOutNode(src_id, candidate);
+          buffer_len[LTID]++;
+        }
       }
+      if (alive)
+        result.SetSampleLength(idx_i, current_itr, subwarp_idx, sample_size);
       active.sync();
-      buffer_1hop.Flush(result.data + result.length_per_sample * idx_i, 0);
-      result.SetSampleLength(idx_i, current_itr, 0, sample_size);
+      for (size_t i = 0; i < 32; i++) {  // LID id
+        for (size_t j = active.thread_rank(); j < buffer_len[(WID)*32 + i];
+             j++) {
+          *result.GetDataPtr(idxMap[(WID)*32 + i], current_itr + 1,
+                             active.thread_rank()) =
+              buffer[(WID)*32 + i][active.thread_rank()];
+        }
+      }
     }
   }
 }
@@ -84,11 +105,12 @@ static __global__ void sample_kernel_2hop_buffer(Sampler_new *sampler) {
   curandState state;
   curand_init(TID, 0, 0, &state);
   __shared__ matrixBuffer<BLOCK_SIZE, 10, uint> buffer_1hop;
-  __shared__ matrixBuffer<BLOCK_SIZE, 25, uint> buffer_2hop;  // not necessary
+  // __shared__ matrixBuffer<BLOCK_SIZE, 25, uint> buffer_2hop;  // not
+  // necessary
   __shared__ uint idxMap[BLOCK_SIZE];
   idxMap[LTID] = 0;
   buffer_1hop.Init();
-  buffer_2hop.Init();
+  // buffer_2hop.Init();
 
   size_t idx_i = TID;
   if (idx_i < result.size)  // for 2-hop, hop_num=3
@@ -128,12 +150,13 @@ static __global__ void sample_kernel_2hop_buffer(Sampler_new *sampler) {
         for (size_t k = active.thread_rank(); k < sample_size;
              k++) {  // get 2hop for 1hop neighbors for each thread
           uint candidate = (int)floor(curand_uniform(&state) * src_degree);
-          // *result.GetDataPtr(idxMap[(WID)*32 + i], current_itr + 1,
-          //                    active.thread_rank()) =
-          //     graph->getOutNode(src_id, candidate);
-          buffer_2hop.Set(graph->getOutNode(src_id, candidate));
+          *result.GetDataPtr(idxMap[(WID)*32 + i], current_itr + 1,
+                             active.thread_rank()) =
+              graph->getOutNode(src_id, candidate);
+          // buffer_2hop.Set(graph->getOutNode(src_id, candidate));
         }
-        buffer_2hop.Flush(result.data + result.length_per_sample * idxMap[(WID)*32 + i] + j*result.hops[current_itr] , 0);
+        // buffer_2hop.Flush(result.data + result.length_per_sample *
+        // idxMap[(WID)*32 + i] + j*result.hops[current_itr] , 0);
 
         if (local.thread_rank() == 0) {
           result.SetSampleLength(idxMap[(WID)*32 + i], current_itr, j,
@@ -218,12 +241,20 @@ float UnbiasedSample(Sampler_new &sampler) {
   cudaMalloc(&size_d, sizeof(uint));
 #pragma omp barrier
   start_time = wtime();
-  if (FLAGS_buffer)
-    sample_kernel_2hop_buffer<<<sampler.result.size / BLOCK_SIZE + 1,
-                                BLOCK_SIZE, 0, 0>>>(sampler_ptr);
-  else
-    sample_kernel_2hop<<<sampler.result.size / BLOCK_SIZE + 1, BLOCK_SIZE, 0,
-                         0>>>(sampler_ptr);
+  if (FLAGS_peritr) {
+    sample_kernel_first<<<sampler.result.size / BLOCK_SIZE + 1, BLOCK_SIZE, 0,
+                          0>>>(sampler_ptr,0);
+    sample_kernel_second<16>
+        <<<sampler.result.size * 16 / BLOCK_SIZE + 1, BLOCK_SIZE, 0, 0>>>(
+            sampler_ptr, 1);
+  } else {
+    if (FLAGS_buffer)
+      sample_kernel_2hop_buffer<<<sampler.result.size / BLOCK_SIZE + 1,
+                                  BLOCK_SIZE, 0, 0>>>(sampler_ptr);
+    else
+      sample_kernel_2hop<<<sampler.result.size / BLOCK_SIZE + 1, BLOCK_SIZE, 0,
+                           0>>>(sampler_ptr);
+  }
 
   CUDA_RT_CALL(cudaDeviceSynchronize());
   // CUDA_RT_CALL(cudaPeekAtLastError());
